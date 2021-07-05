c358212b3899efb50f5ad336a020356cc4d8dc9f
surikunal <suri.kunal.222@gmail.com>
2021-07-05 15:21:17 +0530

    leetcode 1134

---
+++ 1134.java
@@ -0,0 +1,18 @@
+class Solution {
+
+  static boolean armstrongNumber(int n) {
+    int count = 0;
+    int temp = n;
+    while (temp != 0) {
+      count++;
+      temp /= 10;
+    }
+    int ans = 0;
+    int t = n;
+    while (n != 0) {
+      ans += Math.pow(n % 10, count);
+      n /= 10;
+    }
+    return (ans == t);
+  }
+}
