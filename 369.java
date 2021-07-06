3fecae8fb7bebb6de79be7324fb71c4df6528e72
surikunal <suri.kunal.222@gmail.com>
2021-07-06 09:14:32 +0530

    Plus One LinkedList

---
+++ 369.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy, notNine = dummy;
+        while (curr != null) {
+            if (curr.val != 9)
+                notNine = curr;
+            curr = curr.next;
+        }
+        notNine.val++;
+        notNine = notNine.next;
+        while (notNine != null) {
+            notNine.val = 0;
+            notNine = notNine.next;
+        }
+        return dummy.val == 0 ? dummy.next : dummy;
+    }
+}
