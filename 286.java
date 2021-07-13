6348d031794517f472d43e5a3595d3ce1066a893
surikunal <suri.kunal.222@gmail.com>
2021-07-13 09:45:29 +0530

    Walls And Gates

---
+++ 286.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+// import java.util.Pair;
+class Solution {
+    public void wallsAndGates(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+            return;
+
+        int n = grid.length, m = grid[0].length;
+        LinkedList<Integer> que = new LinkedList<>();
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    que.addLast(i * m + j);
+                }
+            }
+        }
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int idx = que.removeFirst();
+                int sr = idx / m, sc = idx % m;
+
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+
+                    if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 2147483647) {
+                        grid[r][c] = grid[sr][sc] + 1;
+                        que.addLast(r * m + c);
+                    }
+                }
+            }
+        }
+    }
+}
