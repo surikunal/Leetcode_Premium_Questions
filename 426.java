7de27fa4a2bde749a47ebb68a13446149422cd50
surikunal <suri.kunal.222@gmail.com>
2021-07-30 10:27:02 +0530

    Binary Search Tree To Doubly LinkedList and To Cyclic Doubly LinkedList

--- 426.java
+++ 426.java
@@ -11,6 +11,8 @@ class Solution {
         helper(node.right);
     }
 
+    // ================================RECURSIVE=================================
+    // tree to doubly linked list
     public Node treeToDoublyList(Node root) {
         Node dummy = new Node(-1);
         prev = dummy;
@@ -21,4 +23,83 @@ class Solution {
         dummy.right = root.left = null;
         return head;
     }
+
+    // tree to cyclic doubly linked list
+    Node bTreeToClist(Node root) {
+        Node dummy = new Node(-1);
+        prev = dummy;
+
+        helper(root);
+
+        Node head = dummy.right;
+        dummy.right = head.left = null;
+
+        head.left = prev;
+        prev.right = head;
+
+        return head;
+    }
+    // ================================ITERARIVE=================================
+    // tree to doubly linked list
+
+    void insertAllLeft(LinkedList<Node> st, Node node) {
+        while (node != null) {
+            st.addFirst(node);
+            node = node.left;
+        }
+    }
+
+    Node bToDLL(Node root) {
+        Node dummy = new Node(-1);
+        Node prev = dummy;
+
+        LinkedList<Node> st = new LinkedList<>();
+        insertAllLeft(st, root);
+
+        while (st.size() != 0) {
+            Node curr = st.removeFirst();
+            prev.right = curr;
+            curr.left = prev;
+            prev = curr;
+
+            insertAllLeft(st, curr.right);
+        }
+        Node head = dummy.right;
+
+        dummy.right = head.left = null;
+        return head;
+    }
+
+    // tree to cyclic doubly linked list
+    void insertAllLeft(LinkedList<Node> st, Node node) {
+        while (node != null) {
+            st.addFirst(node);
+            node = node.left;
+        }
+    }
+
+    Node bToDLL(Node root) {
+        Node dummy = new Node(-1);
+        Node prev = dummy;
+
+        LinkedList<Node> st = new LinkedList<>();
+        insertAllLeft(st, root);
+
+        while (st.size() != 0) {
+            Node curr = st.removeFirst();
+            prev.right = curr;
+            curr.left = prev;
+            prev = curr;
+
+            insertAllLeft(st, curr.right);
+        }
+        Node head = dummy.right;
+        dummy.right = head.left = null;
+
+        prev.right = head;
+        head.left = prev;
+
+        return head;
+    }
+
 }
