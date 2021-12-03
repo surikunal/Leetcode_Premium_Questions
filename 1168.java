19a9e015fe617456efa6ac5dfbe9f7a6fc52583f
Supreme <suri.kunal.222@gmail.com>
2021-12-03 17:27:32 +0530

    Update 1168.java

--- 1168.java
+++ 1168.java
@@ -1,3 +1,5 @@
+// Using UNION FIND     =======================================================
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,5 +43,64 @@ public class 1168 {
         int[] wells = {1,2,2};
         int[][] pipes = {{1,2,1},{2,3,1}};
         System.out.println(minCostToSupplyWater(n, wells, pipes));
+    }
+}
+
+
+// Using PRIMS ALGORITHM    ================================================
+
+class Solution {
+    public class Pair implements Comparable<Pair> {
+        int vtx;
+        int wt;
+        Pair(int vtx, int wt) {
+            this.vtx = vtx;
+            this.wt = wt;
+        }
+        
+        @Override
+        public int compareTo(Pair o) {
+            return this.wt - o.wt;
+        }
+    }
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        // graph construction
+        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; ++i)
+            graph.add(new ArrayList<>());
+        // graph from pipes
+        for (int i = 0; i < pipes.length; ++i) {
+            int u = pipes[i][0];
+            int v = pipes[i][1];
+            int wt = pipes[i][2];
+            graph.get(u).add(new Pair(v, wt));
+            graph.get(v).add(new Pair(u, wt));
+        }
+        // graph from wells
+        for (int i = 1; i <= n; ++i) {
+            graph.get(0).add(new Pair(i, wells[i - 1]));
+            graph.get(i).add(new Pair(0, wells[i - 1]));
+        }
+        
+        // now prims algo using PQ
+        int ans = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        boolean[] vis = new boolean[n + 1];
+        while (pq.size() > 0) {
+            Pair rem = pq.remove();
+            if (vis[rem.vtx] == true) continue;
+            
+            ans += rem.wt;
+            
+            vis[rem.vtx] = true;
+            ArrayList<Pair> nbrs = graph.get(rem.vtx);
+            for (Pair nbr : nbrs) {
+                if (vis[nbr.vtx] == false) {
+                    pq.add(nbr);
+                }
+            }
+        }
+        return ans;
     }
 }
