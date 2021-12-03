23548d120af8e39423c15433ccc4640bff3978cc
Supreme <suri.kunal.222@gmail.com>
2021-12-03 21:04:51 +0530

    Update 1176.java

--- 1176.java
+++ 1176.java
@@ -1,22 +1,17 @@
-public class Main {
-    public static int dietPlanPerformance(int[] arr, int k, int lower, int upper) {
-        int i = 0, ans = 0, count = 0;
-        while (i < arr.length) {
-            if (i % k == 0) {
-                ans = arr[i];
-                if (ans < lower) count--;
-                else if (ans > upper) count++;
-            } else {
-                ans += arr[i];
+
+// very poorly explained question of leetcode
+
+class Solution {
+    public int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
+        int sum = 0, points = 0;
+        for (int i = 0; i < calories.length; ++i) {
+            sum += calories[i];
+            if (i >= k - 1) {
+                if (i > k - 1) sum -= calories[i - k];
+                if (sum > upper) points++;
+                else if (sum < lower) points--;
             }
-            i++;
         }
-        return count;
-    }
-    public static void main(String[] args) {
-        int[] calories = {6,5,0,0};
-        int k = 2, lower = 1, upper = 5;
-        int ans = dietPlanPerformance(calories, k, lower, upper);
-        System.out.println(ans);
+        return points;
     }
 }
