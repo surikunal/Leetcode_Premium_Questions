bb06dece8bd11286ed0cfa35a34362f3d1d81423
Supreme <suri.kunal.222@gmail.com>
2021-12-03 14:59:59 +0530

    Update 760.java

--- 760.java
+++ 760.java
@@ -2,13 +2,11 @@ import java.util.HashMap;
 class Solution {
     public int[] anagramMappings(int[] A, int[] B) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < A.length; ++i) {
-            map.put(A[i], i);
-        }
-        int[] ans = new int[A.length];
-        for (int i = 0; i < B.length; ++i) {
-            ans[i] = map.get(B[i]);            
-        }
+        for (int i = 0; i < nums2.length; ++i)
+            map.put(nums2[i], i);
+        int[] ans = new int[nums2.length];
+        for (int i = 0; i < nums1.length; ++i)
+            ans[i] = map.get(nums1[i]);
         return ans;
     }
 }
