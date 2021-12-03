9302cdea216238fb4613cfbb2319246098d30ae4
Supreme <suri.kunal.222@gmail.com>
2021-12-03 21:50:38 +0530

    Update 1570.java

--- 1570.java
+++ 1570.java
@@ -1,10 +1,34 @@
-public class 1570 {
-    public int dotProduct(int[] a, int[] b) {
-        if (a.length != b.length) return -1;
+class SparseVector {
+    int[] arr;
+    SparseVector(int[] nums) {
+        arr = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
         int ans = 0;
-        for (int i = 0; i < a.length; i++) {
-            ans += a[i] * b[i];
-        }
+        for (int i = 0; i < arr.length; i++)
+            ans += arr[i] * vec.arr[i];
+        return ans;
+    }
+}
+
+//==================================================
+
+class SparseVector {
+    Map<Integer, Integer> map;
+    SparseVector(int[] nums) {
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i)
+            if (nums[i] != 0) map.put(i, nums[i]);
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int ans = 0;
+        for (int i: this.map.keySet())
+            if (vec.map.containsKey(i))
+                ans += this.map.get(i) * vec.map.get(i);
         return ans;
     }
 }
