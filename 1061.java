2c16f527ae2be5f34d66519b6842dcb5321e73a5
Supreme <suri.kunal.222@gmail.com>
2021-12-03 16:05:58 +0530

    Update 1061.java

--- 1061.java
+++ 1061.java
@@ -1,22 +1,20 @@
-import java.util.*;
-public class 1061{
+class Solution {
+    private int findPar(int i, int[] par) {
+        if (i == par[i]) return i;
+        return par[i] = findPar(par[i], par);
+    }
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
         int[] par = new int[26];
-        for (int i = 0; i < 26; ++i) {
-            par[i] = i;
-        }
+        for (int i = 0; i < 26; ++i) par[i] = i;
         for (int i = 0; i < s1.length(); ++i) {
-            int par1 = findPar(s1.charAt(i) - 'a');
-            int par2 = findPar(s2.charAt(i) - 'a');
+            int par1 = findPar(s1.charAt(i) - 'a', par);
+            int par2 = findPar(s2.charAt(i) - 'a', par);
             par[par1] = Math.min(par1, par2);
             par[par2] = Math.min(par1, par2);
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < baseStr.length(); ++i) {
-            char ch = (char) (findPar(baseStr.charAt(i) - 'a') + 'a');
-            sb.append(ch);
-        }
-        return sb.toString();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < baseStr.length(); ++i)
+            ans.append((char)(findPar(baseStr.charAt(i) - 'a', par) + 'a'));
+        return ans.toString();
     }
-};
+}
