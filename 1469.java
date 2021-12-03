87ba6217d03a4e63185d6c8a6357eef8c2d8ce56
Supreme <suri.kunal.222@gmail.com>
2021-12-03 21:25:03 +0530

    Update 1469.java

--- 1469.java
+++ 1469.java
@@ -1,40 +1,55 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-class Node
-{
-    int data;
-    Node left, right;
-
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // recursive
+    public void count(TreeNode node, List<Integer> ans) {
+        if (node == null) return;
+        if (node.left != null && node.right == null) ans.add(node.left.val);
+        if (node.right != null && node.left == null) ans.add(node.right.val);
+        count(node.left, ans);
+        count(node.right, ans);
     }
-}
-class Tree
-{
-    ArrayList<Integer> noSibling(Node node)
-    {
-        // code here
-        if (node == null) return new ArrayList<>(Arrays.asList(-1));
-        Queue<Node> que = new LinkedList<>();
-        que.add(node);
-        ArrayList<Integer> ans = new ArrayList<>();
+    
+    // iterative
+    public void count1(TreeNode root, List<Integer> ans) {
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
         while (!que.isEmpty()) {
-            Node temp = que.poll();
-            if (temp.left != null && temp.right != null) {
-                que.add(temp.left);
-                que.add(temp.right);
-            } else if (temp.left != null) {
-                que.add(temp.left);
-                ans.add(temp.left.data);
-            } else if (temp.right != null) {
-                que.add(temp.right);
-                ans.add(temp.right.data);
+            TreeNode node = que.poll();
+            if (node.left != null && node.right != null) {
+                que.add(node.left);
+                que.add(node.right);
+            } else if (node.left != null) {
+                que.add(node.left);
+                ans.add(node.left.val);
+            } else if (node.right != null) {
+                que.add(node.right);
+                ans.add(node.right.val);
             }
         }
-        return ans.isEmpty() ? new ArrayList<>(Arrays.asList(-1)) : ans;
+    }
+    
+    public List<Integer> getLonelyNodes(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            ans.add(0);
+            return ans;
+        }
+        // count(root, ans);
+        count1(root, ans);
+        return ans;
     }
 }
