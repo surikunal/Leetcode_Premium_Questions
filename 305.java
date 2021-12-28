7bb91078bfea16a7e04f597619c329a454ae8f43
Supreme <suri.kunal.222@gmail.com>
2021-12-28 14:49:51 +0530

    Update 305.java

--- 305.java
+++ 305.java
@@ -1,32 +1,53 @@
-public class 305 {
-    private int findPar(int i, int[] par) {
+class Solution {
+    public int findPar(int i, int[] par) {
         if (i == par[i]) return i;
         return par[i] = findPar(par[i], par);
     }
+    public void union(int i, int j, int[] rank, int[] par) {
+        if (rank[i] < rank[j]) {
+            par[i] = j;
+        } else if (rank[i] > rank[j]) {
+            par[j] = i;
+        } else {
+            par[i] = j;
+            rank[j]++;
+        }
+    }
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        List<Integer> ans = new ArrayList<>();
-        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int[] par = new int[m * n];
+        List<Integer> ans = new ArrayList<Integer>();
+        if (positions == null || positions.length == 0) return ans;
+        int[] par = new int[n * m];
+        int[] rank = new int[n * m];
         Arrays.fill(par, -1);
+        Arrays.fill(rank, 1);
         int count = 0;
-        for (int[] p: positions) {
-            int i = p[0], j = p[1];
-            if (par[i * m + j] == -1) {
-                count++;
-                par[i * m + j] = i * m + j;
-                int par1 = findPar(i * m + j, par);
-                for (int d = 0; d < dir.length; ++d) {
-                    int r = i + dir[d][0];
-                    int c = j + dir[d][1];
-                    if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] != -1) {
-                        int par2 = findPar(r * m + c, par);
-                        if (par1 != par2) {
-                            par[par2] = par1;   // must make p2 as global parent
-                            count--;
-                        }
+        int[][] dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for (int[] position: positions) {
+            int i = position[0], j = position[1];
+            int idx = i * n + j;
+            System.out.println(i + "->" + j);
+            if (par[idx] != -1) {
+                ans.add(count);
+                continue;
+            }
+            count++;
+            par[idx] = idx;
+            // int par1 = findPar(idx, par);
+            int par1 = idx;
+            for (int[] d: dir) {
+                int x = i + d[0], y = j + d[1];
+                if (x >= 0 && y >= 0 && x < m && y < n && par[x * n + y] != -1) {
+                    System.out.println(x + ", " + y);
+                    int nbr = x * n + y;
+                    int par2 = findPar(nbr, par);
+                    if (par1 != par2) {
+                        // union(par1, par2, rank, par);
+                        par[par2] = par1;
+                        count--;
                     }
                 }
             }
+            System.out.println();
             ans.add(count);
         }
         return ans;
