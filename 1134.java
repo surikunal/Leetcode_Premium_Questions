9eedc61015278703776772a5668f86e3e0bab207
Supreme <suri.kunal.222@gmail.com>
2021-12-29 11:16:15 +0530

    Update 1134.java

--- 1134.java
+++ 1134.java
@@ -1,18 +1,17 @@
 class Solution {
-
-  static boolean armstrongNumber(int n) {
-    int count = 0;
-    int temp = n;
-    while (temp != 0) {
-      count++;
-      temp /= 10;
+    public boolean isArmstrong(int n) {
+        int count = 0;
+        int temp = n;
+        while (temp != 0) {
+          count++;
+          temp /= 10;
+        }
+        int ans = 0;
+        int t = n;
+        while (n != 0) {
+          ans += Math.pow(n % 10, count);
+          n /= 10;
+        }
+        return (ans == t);
     }
-    int ans = 0;
-    int t = n;
-    while (n != 0) {
-      ans += Math.pow(n % 10, count);
-      n /= 10;
-    }
-    return (ans == t);
-  }
 }
