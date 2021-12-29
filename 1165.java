0a1396b5719e2c219c0efbe22164d0eaef82a6f4
Supreme <suri.kunal.222@gmail.com>
2021-12-29 14:31:22 +0530

    Update 1165.java

--- 1165.java
+++ 1165.java
@@ -1,15 +1,15 @@
-public class 1165 {
+class Solution {
     public int calculateTime(String keyboard, String word) {
         int[] map = new int[26];
         for (int i = 0; i < keyboard.length(); ++i) {
             int c = keyboard.charAt(i);
             map[c - 'a'] = i;
         }
-
-        int ans = map[word.charAt(0)];
-        for (int i = 1; i < word.length(); ++i) {
-            ans += Math.abs(map[word.charAt(i)] - prev);
-            prev = map[word.charAt(i)];
+        int prev = 0;
+        int ans = 0;
+        for (int i = 0; i < word.length(); ++i) {
+            ans += Math.abs(map[word.charAt(i) - 'a'] - prev);
+            prev = map[word.charAt(i) - 'a'];
         }
         return ans;
     }
