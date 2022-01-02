71ba4f9003647badd122255ed69b31620e4f3e97
Supreme <suri.kunal.222@gmail.com>
2022-01-02 17:07:30 +0530

    Update 366.java

--- 366.java
+++ 366.java
@@ -18,3 +18,10 @@ public class Solution {
         return ans;
     } 
 }
+
+
+For this question we need to take bottom-up approach. The key is to find the height of each node. Here the definition of height is:
+The height of a node is the number of edges from the node to the deepest leaf. --CMU 15-121 Binary Trees
+
+I used a helper function to return the height of current node. According to the definition, the height of leaf is 0. h(node) = 1 + max(h(node.left), h(node.right)).
+The height of a node is also the its index in the result list (res). For example, leaves, whose heights are 0, are stored in res[0]. Once we find the height of a node, we can put it directly into the result.
