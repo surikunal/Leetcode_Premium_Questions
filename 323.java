e1553ae0ce11098ea5a0cab08b90770640463517
Supreme <suri.kunal.222@gmail.com>
2022-01-22 16:20:56 +0530

    Update 323.java

--- 323.java
+++ 323.java
@@ -23,3 +23,35 @@ class Solution {
         return par[i] = find(par[i], par);
     }
 }
+
+// with making graph =========================================
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; ++i) graph.add(new ArrayList<>());
+        for (int[] e: edges) {
+            int u = e[0], v = e[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        boolean[] vis = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (vis[i] == false) {
+                count++;
+                dfs(graph, i, vis);
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(List<List<Integer>> graph, int i, boolean[] vis) {
+        vis[i] = true;
+        for (int nbr: graph.get(i)) {
+            if (vis[nbr] == false) {
+                dfs(graph, nbr, vis);
+            }
+        }
+    }
+}
